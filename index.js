1999e9241327f0a6aa6800b194ecd827781ea924
arieldavis22 <aadavisbusiness@gmail.com>
2020-03-29 16:43:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(drivers, strings) {
+    const arr = [];
+    for (const user of drivers) {
+        if (user.toLowerCase() === strings.toLowerCase()) {
+            arr.push(user);
+        }
+    }
+    return arr;
+}
+
+function fuzzyMatch(drivers, strings) {
+    return drivers.filter(function (char) {return strings[0] === char[0]});
+}
+
+function matchName(list, name) {
+    return list.filter(function (driver) {
+        return driver.name === name
+    });
+}
