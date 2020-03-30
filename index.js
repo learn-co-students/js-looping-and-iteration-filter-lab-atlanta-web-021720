284cbbfa515be9c64148b0b07536e9017cf703cd
SpencerBlum <spencerblum@gmail.com>
2020-03-29 21:27:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+
+function findMatching(list, name){
+   let array = [];
+   for (const user of list){ 
+       if (user.toLowerCase() == name.toLowerCase()){
+           array.push(user)
+       }}
+return array
+    
+}
+
+function fuzzyMatch(list, name){
+
+    return list.filter(function(char){return name[0] === char [0]});
+}
+
+function matchName(data, name) {
+    return data.filter(function(driver){
+        return driver.name === name
+    })
+}
